37efda5d86d62317b62a055033a84f8ff7c845c3
Vilen <vilenmoscow@gmail.com>
2021-04-21 01:12:56 +0300

    Update main.py

--- ezcodes/smile_rand/main.py
+++ ezcodes/smile_rand/main.py
@@ -1,5 +1,6 @@
 import random
+n = 100  #  the number of smiles needed to be generated
 s = '💞💓😍🥰❤😘😻💕💗'
 s = list(s)
-for _ in range(100):
+for _ in range(n):
     print(s[random.randint(0,8)],end='')
